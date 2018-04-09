a06d8d494cfc415eb6195302bb299eeb58a39bc8
rwkregime <38169451+rwkregime@users.noreply.github.com>
2018-04-08 19:28:51 -0700

    Add files via upload
    update from hello world to bot v1.0

--- worker.py
+++ worker.py
@@ -1,18 +1,27 @@
-    import discord
-    import asyncio
+import json, discord, random
 
-    client = discord.Client()
+# start discord client
+client = discord.Client()
 
-    @client.event
-    async def on_ready():
-        print('Logged in as')
-        print(client.user.name)
-        print(client.user.id)
-        print('------')
+# load file into json
+o = json.load(open('sd.json','rb'))
 
-    @client.event
-    async def on_message(message):
-        if message.content == "Hello":
-            await client.send_message(message.channel, "World")
+# strip json-shit we dont want
+quotes = [m['content'] for m in o if int(m['author']['id']) ==  315864528774627338]
 
-    client.run(NDMyMDM3Nzg3MTI3MjUwOTU0.DannQw.RPA7NHrXaRutfWGtZsPNEdYjBR8)
+# ditch o object since we're done with it..
+del o
+
+@client.event
+async def on_message(message):
+
+    if message.content == '!sd':
+        msg = '{}'.format(random.choice(quotes))
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as', client.user.name,client.user.id)
+
+if __name__ == "__main__":
+    client.run('NDMyMDM3Nzg3MTI3MjUwOTU0.DannQw.RPA7NHrXaRutfWGtZsPNEdYjBR8')
