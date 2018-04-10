38e0fb981d4501f044944250ab5c65d048687e8c
Juji <38169451+rwkregime@users.noreply.github.com>
2018-04-10 02:26:51 -0700

    Update worker.py
    added search function
    
    !search "term" is the trigger.
    Shows 4 timestamped results per request

--- worker.py
+++ worker.py
@@ -1,27 +1,48 @@
-import json, discord, random
+import discord, random, json, datetime
+from discord.ext import commands
 
-# start discord client
-client = discord.Client()
+# YO! JUJI! pay attention to this "labmda expression"
+fix_date = lambda o: datetime.datetime.strptime(o, "%Y-%m-%dT%H:%M").strftime('%m/%d %h:%M')
 
-# load file into json
+# create bot instance
+bot = commands.Bot(command_prefix='!', description='SD Memorial Bot')
+
+# load file into json, strip non-SD comments then del 'o' object
 o = json.load(open('sd.json','rb'))
-
-# strip json-shit we dont want
-quotes = [m['content'] for m in o if int(m['author']['id']) ==  315864528774627338]
-
-# ditch o object since we're done with it..
+quotes = [(fix_date(m['timestamp'][:16]),m['content']) for m in o if int(m['author']['id']) ==  315864528774627338]
 del o
 
-@client.event
-async def on_message(message):
-
-    if message.content == '!sd':
-        msg = '{}'.format(random.choice(quotes))
-        await client.send_message(message.channel, msg)
-
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as', client.user.name,client.user.id)
+    channel = discord.Object(id=432066274336440330)
+    print("Logged in as:  name={}, id={}".format( bot.user.name, bot.user.id ))
+    
+
+
+@bot.command(pass_context=True)
+async def sd(ctx):
+    """Chooses a random quote."""
+    timestamp, content = random.choice(quotes)
+    msg = "{}: {}".format(timestamp, content)
+    await bot.say(msg)
+
+
+@bot.command(pass_context=True)
+async def search(ctx, phrase : str):
+    """Match quotes with a search-phrase"""
+    results = []
+    for timestamp, content in quotes:
+        if phrase in content: results.append( "{}: {}".format(timestamp, content) )
+        if len(results) > 3: break
+
+    if len(results) > 0:
+        msg = "Results:\n\n{}".format( "\n".join(results) )
+    else:
+        msg = "no results for `{}`".format(phrase)
+    await bot.say(msg)
 
 if __name__ == "__main__":
-    client.run('NDMyMDM3Nzg3MTI3MjUwOTU0.DannQw.RPA7NHrXaRutfWGtZsPNEdYjBR8')
+    try:
+        bot.run('NDMyMDM3Nzg3MTI3MjUwOTU0.DannQw.RPA7NHrXaRutfWGtZsPNEdYjBR8')
+    except:
+        pass
